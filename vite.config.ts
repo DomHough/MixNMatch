82c0680ec13cb233421c922d4bccfda0d7b37266
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2025-11-04 21:15:39 +0000

    refactor: remove unused noAttr function from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-const noAttr = () => {
-  return {
-    name: "no-attribute",
-    transformIndexHtml(html: string) {
-      return html.replace(`type="module" crossorigin`, "defer").replace(`crossorigin `, "");
-    }
-  }
-}
+// const noAttr = () => {
+//   return {
+//     name: "no-attribute",
+//     transformIndexHtml(html: string) {
+//       return html.replace(`type="module" crossorigin`, "defer").replace(`crossorigin `, "");
+//     }
+//   }
+// }
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), noAttr()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: './dist',
-    assetsDir: './'
+    assetsDir: './',
+    rollupOptions: { output: { manualChunks: undefined } },
   },
   base: './',
-  publicDir: 'public'
+  publicDir: 'public',
 })
