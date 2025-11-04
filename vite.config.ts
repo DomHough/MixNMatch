52c92186626e494c82b826e9984e07cbc9fe849e
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2025-11-04 20:26:52 +0000

    feat: add build and deploy workflow with S3 deployment and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+const noAttr = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html: string) {
+      return html.replace(`type="module" crossorigin`, "defer").replace(`crossorigin `, "");
+    }
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), noAttr()],
+  build: {
+    outDir: './dist',
+    assetsDir: './'
+  },
+  base: './',
+  publicDir: 'public'
 })
